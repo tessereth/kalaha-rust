33984c9cb617dc47ea45b1cb00f5c664c9b60bc4
tessereth <teresa.bradbury@bugcrowd.com>
2017-12-26 17:19:42 +1100

    Tidy up main file

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,6 @@ use kalaha::ai;
 
 fn main() {
     let mut game = Kalaha::new();
-    println!("{}", game);
-    game.choose(0);
-    println!("{}", game);
-    println!("Valid move 0: {:?}", game.valid_move(0));
-    println!("Valid move 1: {:?}", game.valid_move(1));
-    game.choose(1);
-    println!("{}", game);
-    game.choose(1);
-    println!("{}", game);
 
     let ai_a = ai::FirstValid {};
     let ai_b = ai::LastValid {};
